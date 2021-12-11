dce55b8c331a7e7e51e3429c86e5a33bd5836b22
Jixu Hu <jixu.hjx@alibaba-inc.com>
2021-12-11 12:36:09 +0800

    add certs

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,13 @@
+: "${CS_ID:=}"
+: "${CS_TEAM:=}"
+
 xcodebuild -resolvePackageDependencies -workspace Clipy.xcworkspace -scheme Clipy -configuration Release -clonedSourcePackagesDirPath SourcePackages
 
-set -o pipefail && xcodebuild -workspace Clipy.xcworkspace -scheme Clipy -configuration Release -clonedSourcePackagesDirPath SourcePackages -destination 'generic/platform=macOS' -archivePath Clipy.xcarchive CODE_SIGN_IDENTITY='' DEVELOPMENT_TEAM='' clean archive | xcpretty
+set -o pipefail && xcodebuild -workspace Clipy.xcworkspace -scheme Clipy -configuration Release -clonedSourcePackagesDirPath SourcePackages -destination 'generic/platform=macOS' -archivePath Clipy.xcarchive CODE_SIGN_IDENTITY="${CS_ID}" DEVELOPMENT_TEAM="${CS_TEAM}" clean archive | xcpretty
 
-rm -rf Clipy.app* || true && cp -rfv Clipy.xcarchive/Products/Applications/Clipy.app ./
-
-zip -ry Clipy.app.zip Clipy.app
+# Clean up
+rm -rf Clipy.app* || true
+working_dir=$(pwd)
+# Copy
+(cd Clipy.xcarchive/Products/Applications && zip -ry "${working_dir}/Clipy.app.zip" Clipy.app)
+unzip Clipy.app.zip
